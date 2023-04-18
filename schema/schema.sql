abf154d6d4ba36de14fa1b34362757db76ba512a
Achira-Oulu <achira.hendalage@student.oulu.fi>
2023-04-18 17:40:17 +0300

    templateisssue

--- schema/schema.sql
+++ schema/schema.sql
@@ -10,7 +10,6 @@ CREATE TABLE `recipient` (
   PRIMARY KEY (`recipient_id`)
 );
 
-
 CREATE TABLE email_notification_audit (
   email_notification_audit_id int NOT NULL AUTO_INCREMENT,
   email_subject varchar(255) NOT NULL DEFAULT '',
@@ -24,3 +23,21 @@ PRIMARY KEY (email_notification_audit_id)
 
 ALTER TABLE email_notification_audit
 ADD FOREIGN KEY (recipient_id) REFERENCES recipient(recipient_id);
+
+
+
+CREATE TABLE template (
+  template_id int NOT NULL AUTO_INCREMENT,
+  template_name varchar(255) NOT NULL DEFAULT '',
+  template_body json DEFAULT NULL,
+  `active` tinyint NOT NULL DEFAULT '1',
+  created_date timestamp NOT NULL,
+  modified_date timestamp NULL DEFAULT NULL,
+  template_subject varchar(255) DEFAULT NULL,
+PRIMARY KEY (template_id)
+);
+
+ALTER TABLE email_notification_template
+ADD FOREIGN KEY (recipient_id) REFERENCES recipient(recipient_id);
+
+
