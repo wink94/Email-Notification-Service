55a5436bec00505a4e1e847f397af0be32d2395b
Achira-Oulu <achira.hendalage@student.oulu.fi>
2023-04-18 17:37:36 +0300

    template with issue

---
+++ src/dao/templateDao.js
@@ -0,0 +1,38 @@
+import DBException from '../exceptions/DBException';
+import { getModule } from '../models/index';
+
+class TemplateDao {
+  async insertTemplate(dataParams) {
+    try {
+      const template = getModule('template');
+      return template.create(
+        dataParams,
+      );
+    } catch (error) {
+      throw new DBException(error.toString());
+    }
+  }
+
+  async getAllTemplate() {
+    try {
+      const template = getModule('template');
+      return template.findAll({ raw: true, subQuery: false });
+    } catch (error) {
+      throw new DBException(error.toString());
+    }
+  }
+
+  async getTemplate(templateId) {
+    try {
+      const template = getModule('template');
+      return template.findAll({
+        where: { templateId },
+        raw: true,
+      });
+    } catch (error) {
+      throw new DBException(error.toString());
+    }
+  }
+}
+
+export default new TemplateDao();
