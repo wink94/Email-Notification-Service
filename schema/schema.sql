9ebff4959b59d585c8cee70e77423d8b44dcfb2c
windula_k <mwindula16@gmail.com>
2023-04-19 18:17:38 +0530

    schema template

--- schema/schema.sql
+++ schema/schema.sql
@@ -24,3 +24,14 @@ PRIMARY KEY (email_notification_audit_id)
 
 ALTER TABLE email_notification_audit
 ADD FOREIGN KEY (recipient_id) REFERENCES recipient(recipient_id);
+
+CREATE TABLE `template` (
+  `template_id` int NOT NULL AUTO_INCREMENT,
+  `template_name` varchar(255) NOT NULL DEFAULT '',
+  `template_body` json DEFAULT NULL,
+  `active` tinyint NOT NULL DEFAULT '1',
+  `created_date` timestamp NOT NULL,
+  `modified_date` timestamp NULL DEFAULT NULL,
+  `template_subject` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`template_id`)
+)
