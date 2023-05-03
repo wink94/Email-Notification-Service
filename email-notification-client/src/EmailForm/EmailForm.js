28426dfc9d42f119e7ca1f646de2a9a253e03266
Kavindu Ravishan <125926016+kravishan@users.noreply.github.com>
2023-05-03 15:32:59 +0300

    Update EmailForm.js

--- email-notification-client/src/EmailForm/EmailForm.js
+++ email-notification-client/src/EmailForm/EmailForm.js
@@ -7,27 +7,52 @@ import TemplateWindow from './TemplatePopup.js';
 
 
 function EmailForm() {
-  const [to, setTo] = useState("");
+  const [applicationName, setApplicationName] = useState("");
+  const [emailCategory, setEmailCategory] = useState("");
+  const [templateId, setTemplateId] = useState("");
+  const [templateData, setTemplateData] = useState("");
   const [subject, setSubject] = useState("");
-  const [body, setBody] = useState("");
+  const [name, setName] = useState("");
+  const [product, setProduct] = useState("");
+  const [recipientId, setRecipientId] = useState("");
   const [showTemplatePopup, setShowTemplatePopup] = useState(false);
   const [showRecipientPopup, setShowRecipientPopup] = useState(false);
 
-  const handleToChange = (event) => {
-    setTo(event.target.value);
+  const handleApplicationNameChange = (event) => {
+    setApplicationName(event.target.value);
+  };
+
+  const handleEmailCategoryChange = (event) => {
+    setEmailCategory(event.target.value);
+  };
+
+  const handleTemplateIdChange = (event) => {
+    setTemplateId(event.target.value);
+  };
+
+  const handleTemplateDataChange = (event) => {
+    setTemplateData(event.target.value);
   };
 
   const handleSubjectChange = (event) => {
     setSubject(event.target.value);
   };
 
-  const handleBodyChange = (event) => {
-    setBody(event.target.value);
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleProductChange = (event) => {
+    setProduct(event.target.value);
+  };
+
+  const handleRecipientIdChange = (event) => {
+    setRecipientId(event.target.value);
   };
 
   const handleSendEmail = (event) => {
     event.preventDefault();
-    // TODO: Send email using to, subject, and body
+    // TODO: Send email using applicationName, emailCategory, templateId, templateData, subject, name, product, and recipientId
   };
 
   const handleManageTemplatesClick = () => {
@@ -43,6 +68,7 @@ function EmailForm() {
     setShowRecipientPopup(false);
   };
 
+
   return (
     <div className="email-form">
       <div className="email-form-header">
@@ -54,16 +80,45 @@ function EmailForm() {
       </div>
       <div className="email-form-body">
         <div className="email-form-input">
-          <label htmlFor="to-input">To:</label>
-          <input type="email" id="to-input" value={to} onChange={handleToChange} />
+          <label htmlFor="applicationName-input">Application Name:</label>
+          <input type="text" id="applicationName-input" value={applicationName} onChange={handleApplicationNameChange} />
+        </div>
+        <div className="email-form-input">
+          <label htmlFor="emailCategory-input">Email Category:</label>
+          <input type="text" id="emailCategory-input" value={emailCategory} onChange={handleEmailCategoryChange} />
+        </div>
+        <div className="email-form-input">
+          <label htmlFor="templateId-input">Template Id:</label>
+          <input type="text" id="templateId-input" value={templateId} onChange={handleTemplateIdChange} />
+        </div>      
+        <div className="email-form-input">
+          <label htmlFor="templateData-input">Recipient Id:</label>
+          <input type="text" id="recipientId-input" value={recipientId} onChange={handleRecipientIdChange} />
+        </div>
+        <div className="email-form-input">
+          <label htmlFor="templateData-input">Template Data:</label>
+          <input type="text" id="templateData-input" value={templateData} onChange={handleTemplateDataChange} />
         </div>
         <div className="email-form-input">
           <label htmlFor="subject-input">Subject:</label>
-          <input type="text" id="subject-input" value={subject} onChange={handleSubjectChange} />
+          <div className="subject-input-wrapper">
+            <input type="text" id="subject-input" value={subject} onChange={handleSubjectChange} />
+            <span className="subject-input-icon"></span>
+          </div>
         </div>
         <div className="email-form-input">
-          <label htmlFor="body-input">Body:</label>
-          <textarea id="body-input" rows="10" cols="50" value={body} onChange={handleBodyChange} />
+          <label htmlFor="name-input">Name:</label>
+          <div className="name-input-wrapper">
+            <input type="text" id="name-input" value={name} onChange={handleNameChange} />
+            <span className="name-input-icon"></span>
+          </div>
+        </div>
+        <div className="email-form-input">
+          <label htmlFor="product-input">Product:</label>
+          <div className="product-input-wrapper">
+            <input type="text" id="product-input" value={product} onChange={handleProductChange} />
+            <span className="product-input-icon"></span>
+          </div>
         </div>
         <div className="email-form-footer">
           <button className="send-email-btn" onClick={handleSendEmail}>Send Email</button>
